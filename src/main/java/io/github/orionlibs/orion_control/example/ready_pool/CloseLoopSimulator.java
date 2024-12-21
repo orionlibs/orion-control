735b5fed69ce850f1c8587bd0cce13565d643c99
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-12-21 21:33:45 +0000

    added chart

--- src/main/java/io/github/orionlibs/orion_control/example/ready_pool/CloseLoopSimulator.java
+++ src/main/java/io/github/orionlibs/orion_control/example/ready_pool/CloseLoopSimulator.java
@@ -1,21 +1,35 @@
 package io.github.orionlibs.orion_control.example.ready_pool;
 
+import io.github.orionlibs.orion_simulation.chart.Chart;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CloseLoopSimulator
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         Buffer sys = new Buffer(10, 5);
         Controller c = new Controller(1.25, 0.01);
         int y = 0;
         int iterations = 5000;
+        List<Double> timeSteps = new ArrayList<>();
+        List<Double> objectsInBuffer = new ArrayList<>();
         for(int i = 0; i < iterations; i++)
         {
+            timeSteps.add((double)i);
             int r = setPoint(i);
             int error = r - y;
             double numberOfObjectsToAddToTheReadyPool = c.work(error);
             y = sys.work(numberOfObjectsToAddToTheReadyPool);
+            objectsInBuffer.add((double)y);
             //System.out.println("i = " + i + "--r = " + r + "--error = " + error + "--numberOfObjectsToAddToTheReadyPool = " + numberOfObjectsToAddToTheReadyPool + "--y = " + y);
         }
+
+
+        double[] xData = timeSteps.stream().mapToDouble(d -> d).toArray();
+        double[] yData = objectsInBuffer.stream().mapToDouble(d -> d).toArray();
+        Chart.show2DPlot(xData, yData);
     }
 
 
